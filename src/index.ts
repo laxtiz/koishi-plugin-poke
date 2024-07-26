e3193966c67e60a69b688806cc80e3f9cd696894
Zhong Liu <laxtiz@live.com>
2024-07-26 15:06:34 +0800

    更新koishi插件以支持自定义戳一戳事件响应

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,89 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Session } from "koishi";
 
-export const name = 'poke'
+declare module "koishi" {
+  interface Events {
+    notice(session: Session): void;
+  }
+}
 
-export interface Config {}
+export const name = "poke";
+export const usage = `
+## 自定义戳一戳事件响应
 
-export const Config: Schema<Config> = Schema.object({})
+仅兼容 OneBot 平台，支持 [Lagrange.OneBot] 和 [NapCat]
 
-export function apply(ctx: Context) {
+[Lagrange.OneBot]: https://lagrangedev.github.io/Lagrange.Doc/
+[NapCat]: https://napneko.github.io/zh-CN/
+`;
+
+export interface Config {
+  mode: "command" | "message";
+  command: { content: string; probility: number };
+  messages: { content: string; weight: number }[];
+}
+
+export const Config: Schema<Config> = Schema.intersect([
+  Schema.object({
+    mode: Schema.union([
+      Schema.const("command").description("执行命令"),
+      Schema.const("message").description("回复消息"),
+    ])
+      .default("command")
+      .description("响应模式"),
+  }),
+  Schema.object({
+    command: Schema.object({
+      content: Schema.string().default("status").description("命令内容"),
+      probility: Schema.number()
+        .min(0)
+        .max(100)
+        .role("slider")
+        .default(50)
+        .description("触发概率"),
+    }),
+  }).description("命令内容配置"),
+  Schema.object({
+    messages: Schema.array(
+      Schema.object({
+        content: Schema.string().required().description("消息内容"),
+        weight: Schema.number().min(0).max(100).default(50).description("权重"),
+      }),
+    )
+      .role("table")
+      .default([{ content: "戳你一下", weight: 50 }])
+      .description("消息内容"),
+  }).description("消息内容配置"),
+]);
+
+export function apply(ctx: Context, config: Config) {
   // write your plugin here
+  ctx.on("notice", (session) => {
+    if (session.subtype != "poke") {
+      return;
+    }
+
+    switch (config.mode) {
+      case "command":
+        if (Math.random() * 100 < config.command.probility) {
+          session.execute(config.command.content);
+        }
+        break;
+      case "message":
+        if (config.messages.length > 0)
+          session.sendQueued(randomMessage(config.messages));
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+function randomMessage(messages: { content: string; weight: number }[]) {
+  const totalWeight = messages.reduce((sum, cur) => sum + cur.weight, 0);
+  const random = Math.random() * totalWeight;
+  let sum = 0;
+  for (const message of messages) {
+    sum += message.weight;
+    if (random < sum) return message.content;
+  }
 }
